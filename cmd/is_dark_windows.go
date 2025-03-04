31f6fa16c83a727a7c53acc3cb4dc28c505d2236
Thiago Kenji Okada <thiagokokada@gmail.com>
2025-03-04 20:24:59 +0000

    feat: Improve logDebug

--- cmd/is_dark_windows.go
+++ cmd/is_dark_windows.go
@@ -7,12 +7,14 @@ import (
 func isDarkMode() bool {
 	k, err := registry.OpenKey(registry.CURRENT_USER, `Software\Microsoft\Windows\CurrentVersion\Themes\Personalize`, registry.QUERY_VALUE)
 	if err != nil {
+		logDebug("Debug [registry open key error]: %v", err)
 		return isDarkModeDefault
 	}
 	defer k.Close()
 
 	v, _, err := k.GetIntegerValue("AppsUseLightTheme")
 	if err != nil {
+		logDebug("Debug [get integer value error]: %v", err)
 		return isDarkModeDefault
 	}
 
