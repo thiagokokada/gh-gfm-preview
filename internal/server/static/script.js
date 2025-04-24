7f3f9844f011650fe8af2ab235c78b62a2989c52
Thiago Kenji Okada <thiagokokada@gmail.com>
2025-04-24 08:36:34 +0100

    feat: add jslint

--- internal/server/static/script.js
+++ internal/server/static/script.js
@@ -1,21 +1,27 @@
-"use strict";
+/*jslint browser,long,fart,indent2*/
+/*global alert,console,window,WebSocket*/
 
-// Workaround issue with MermaidJS that doesn't allow changing theme on
-// re-initialization
-// https://github.com/mermaid-js/mermaid/issues/1945#issuecomment-1661264708
-(function(window) {
-  const querySelector = "code.language-mermaid";
+(function () {
+  "use strict";
+
+  const mermaidQuery = "code.language-mermaid";
+  const copyIcon = `<svg class="copy-icon" aria-hidden="true" fill="none" height="18" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="18" style="color:"currentColor";"><path d="M8 17.929H6c-1.105 0-2-.912-2-2.036V5.036C4 3.91 4.895 3 6 3h8c1.105 0 2 .911 2 2.036v1.866m-6 .17h8c1.105 0 2 .91 2 2.035v10.857C20 21.09 19.105 22 18 22h-8c-1.105 0-2-.911-2-2.036V9.107c0-1.124.895-2.036 2-2.036z"></path></svg>`;
+  const tickIcon = `<svg class="tick-icon" aria-hidden="true" fill="none" height="18" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="18" style="color: "currentColor";"><path d="M5 13l4 4L19 7"></path></svg>`;
 
   function loadMermaid(isLight) {
-    const theme = (isLight ? "default" : "dark");
+    const theme = (
+      isLight
+      ? "default"
+      : "dark"
+    );
     window.mermaid.initialize({startOnLoad: false, theme});
-    window.mermaid.run({querySelector});
+    window.mermaid.run({querySelector: mermaidQuery});
   }
 
   function saveOriginalData() {
     return new Promise((resolve, reject) => {
       try {
-        const els = document.querySelectorAll(querySelector);
+        const els = document.querySelectorAll(mermaidQuery);
         els.forEach((element) => {
           element.setAttribute("data-original-code", element.innerHTML);
         });
@@ -29,9 +35,9 @@
   function resetProcessed() {
     return new Promise((resolve, reject) => {
       try {
-        const els = document.querySelectorAll(querySelector);
+        const els = document.querySelectorAll(mermaidQuery);
         els.forEach((element) => {
-          if (element.getAttribute("data-original-code") != null) {
+          if (element.getAttribute("data-original-code") !== null) {
             element.removeAttribute("data-processed");
             element.innerHTML = element.getAttribute("data-original-code");
           }
@@ -43,82 +49,80 @@
     });
   }
 
-  function init() {
+  function initMermaid() {
+    // Workaround issue with MermaidJS that doesn't allow changing theme on
+    // re-initialization
+    // https://github.com/mermaid-js/mermaid/issues/1945#issuecomment-1661264708
     saveOriginalData().catch(console.error);
 
-    if (window.Param.mode == "dark") {
+    if (window.Param.mode === "dark") {
       loadMermaid(true);
-    } else if (window.Param.mode == "light") {
+    } else if (window.Param.mode === "light") {
       loadMermaid(false);
     } else {
       const prefersLightQuery = window.matchMedia("(prefers-color-scheme: light)");
       loadMermaid(prefersLightQuery.matches);
       // Change CSS when the theme changes
       prefersLightQuery.addEventListener("change", (e) => {
-        resetProcessed()
-          .then(loadMermaid(e.matches))
-          .catch(console.error);
+        resetProcessed().then(loadMermaid(e.matches)).catch(console.error);
       });
     }
   }
 
-  window.initMermaid = init;
-})(window);
+  async function loadMarkdown() {
+    const response = await fetch(`/__/md?path=${window.location.pathname.slice(1)}`);
+    const result = await response.text();
+    const markdownBody = document.getElementById("markdown-body");
+    markdownBody.innerHTML = result;
 
-const copyIcon = `<svg class="copy-icon" aria-hidden="true" fill="none" height="18" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="18" style="color:"currentColor";"><path d="M8 17.929H6c-1.105 0-2-.912-2-2.036V5.036C4 3.91 4.895 3 6 3h8c1.105 0 2 .911 2 2.036v1.866m-6 .17h8c1.105 0 2 .91 2 2.035v10.857C20 21.09 19.105 22 18 22h-8c-1.105 0-2-.911-2-2.036V9.107c0-1.124.895-2.036 2-2.036z"></path></svg>`;
-const tickIcon = `<svg class="tick-icon" aria-hidden="true" fill="none" height="18" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="18" style="color: "currentColor";"><path d="M5 13l4 4L19 7"></path></svg>`;
-
-async function loadMarkdown() {
-  const response = await fetch(`/__/md?path=${window.location.pathname.slice(1)}`);
-  const result = await response.text();
-  const markdownBody = document.getElementById("markdown-body");
-  markdownBody.innerHTML = result;
-
-  initMermaid();
-  typesetMathJax();
-  addCopyButtons();
-}
-
-function typesetMathJax() {
-  if (window.MathJax) {
-    MathJax.typeset();
+    initMermaid();
+    typesetMathJax();
+    addCopyButtons();
   }
-}
 
-function addCopyButtons() {
-  document.querySelectorAll(".markdown-body pre").forEach((pre) => {
-    const code = pre.querySelector("code");
-    const button = document.createElement("button");
-    button.classList.add("copy-button");
-    button.setAttribute("aria-label", "Copy code to clipboard");
-    button.innerHTML = copyIcon;
-    pre.style.position = "relative";
-    pre.appendChild(button);
+  function typesetMathJax() {
+    if (window.MathJax) {
+      window.MathJax.typeset();
+    }
+  }
 
-    button.addEventListener("click", () => {
-      navigator.clipboard.writeText(code.textContent).then(() => {
-        button.innerHTML = tickIcon;
-        setTimeout(() => { button.innerHTML = copyIcon; }, 1000);
-      }).catch(() => {
-        alert("Failed to copy");
+  function addCopyButtons() {
+    document.querySelectorAll(".markdown-body pre").forEach((pre) => {
+      const code = pre.querySelector("code");
+      const button = document.createElement("button");
+      button.classList.add("copy-button");
+      button.setAttribute("aria-label", "Copy code to clipboard");
+      button.innerHTML = copyIcon;
+      pre.style.position = "relative";
+      pre.appendChild(button);
+
+      button.addEventListener("click", () => {
+        navigator.clipboard.writeText(code.textContent).then(() => {
+          button.innerHTML = tickIcon;
+          setTimeout(() => {
+            button.innerHTML = copyIcon;
+          }, 1000);
+        }).catch(() => {
+          alert("Failed to copy");
+        });
       });
     });
-  });
-}
-
-(async function(window) {
-  await loadMarkdown();
-
-  if (window.Param.reload) {
-    const conn = new WebSocket(`ws://${window.Param.host}/ws`);
-    conn.onopen = () => conn.send("Ping");
-    conn.onerror = (e) => console.log(`Connection error: ${e}`);
-    conn.onclose = (e) => console.log(`Connection closed: ${e}`);
-    conn.onmessage = (e) => {
-      if (e.data === "reload") {
-        console.log("Reload page!");
-        loadMarkdown();
-      }
-    };
   }
-})(window);
+
+  (async function () {
+    await loadMarkdown();
+
+    if (window.Param.reload) {
+      const conn = new WebSocket(`ws://${window.Param.host}/ws`);
+      conn.onopen = () => conn.send("Ping");
+      conn.onerror = (e) => console.log(`Connection error: ${e}`);
+      conn.onclose = (e) => console.log(`Connection closed: ${e}`);
+      conn.onmessage = (e) => {
+        if (e.data === "reload") {
+          console.log("Reload page!");
+          loadMarkdown();
+        }
+      };
+    }
+  }());
+}());
