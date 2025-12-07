e0b14673a8158a1ec3ce2ffe492f066c230ca078
Thiago Kenji Okada <thiagokokada@gmail.com>
2025-12-07 22:57:14 +0000

    chore: update jslint

--- jslint.sh
+++ jslint.sh
@@ -2,5 +2,10 @@
 
 set -eu
 
-curl -sL https://raw.githubusercontent.com/jslint-org/jslint/refs/tags/v2025.3.31/jslint.mjs > /tmp/jslint.mjs
-node /tmp/jslint.mjs internal/server/static/script.js
+OUTFILE="jslint.mjs"
+VERSION="v2025.10.31"
+SHA256SUM="295b37f861934d48d50dfed7c539b78fe6c3a828027228611bbc396354386a32"
+
+curl -sL "https://raw.githubusercontent.com/jslint-org/jslint/refs/tags/$VERSION/jslint.mjs" -o "$OUTFILE"
+echo "$SHA256SUM $OUTFILE" | sha256sum -c - >/dev/null
+node "$OUTFILE" $@
