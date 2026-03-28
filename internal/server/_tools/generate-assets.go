be14023a22cdc6a7023fadcee954fb99904a2375
Thiago Kenji Okada <thiagokokada@gmail.com>
2026-03-28 18:51:44 +0000

    Revert "Merge pull request #90 from thiagokokada/revert-geojson-topojson"
    This reverts commit a1ae19faa378a843be0a02d55bf232021a1af87b, reversing
    changes made to 18c488b24ef56347e6d3cef4154b3feafb503256.

--- internal/server/_tools/generate-assets.go
+++ internal/server/_tools/generate-assets.go
@@ -57,6 +57,46 @@ func main() {
 		"0d2b6f2361e7e0ce466a6ed458e03daa5584b42ef6926c3beb62eb64670ca261",
 	)
 	getAsset(
+		"https://unpkg.com/leaflet@1.9.4/dist/leaflet.js",
+		assetsDir+"/leaflet.js",
+		"db49d009c841f5ca34a888c96511ae936fd9f5533e90d8b2c4d57596f4e5641a",
+	)
+	getAsset(
+		"https://unpkg.com/leaflet@1.9.4/dist/leaflet.css",
+		assetsDir+"/leaflet.css",
+		"a7837102824184820dfa198d1ebcd109ff6d0ff9a2672a074b9a1b4d147d04c6",
+	)
+	getAsset(
+		"https://unpkg.com/leaflet@1.9.4/dist/images/layers.png",
+		assetsDir+"/images/layers.png",
+		"1dbbe9d028e292f36fcba8f8b3a28d5e8932754fc2215b9ac69e4cdecf5107c6",
+	)
+	getAsset(
+		"https://unpkg.com/leaflet@1.9.4/dist/images/layers-2x.png",
+		assetsDir+"/images/layers-2x.png",
+		"066daca850d8ffbef007af00b06eac0015728dee279c51f3cb6c716df7c42edf",
+	)
+	getAsset(
+		"https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+		assetsDir+"/images/marker-icon.png",
+		"574c3a5cca85f4114085b6841596d62f00d7c892c7b03f28cbfa301deb1dc437",
+	)
+	getAsset(
+		"https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+		assetsDir+"/images/marker-icon-2x.png",
+		"00179c4c1ee830d3a108412ae0d294f55776cfeb085c60129a39aa6fc4ae2528",
+	)
+	getAsset(
+		"https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+		assetsDir+"/images/marker-shadow.png",
+		"264f5c640339f042dd729062cfc04c17f8ea0f29882b538e3848ed8f10edb4da",
+	)
+	getAsset(
+		"https://cdn.jsdelivr.net/npm/topojson-client@3.1.0/dist/topojson-client.min.js",
+		assetsDir+"/topojson-client.min.js",
+		"25cd02ae486cc5063e0215a4e4cfb15de83700c87ac48bac4d57dc6aaf3ebb89",
+	)
+	getAsset(
 		"https://cdnjs.cloudflare.com/ajax/libs/mathjax/3.2.2/es5/tex-mml-chtml.min.js",
 		assetsDir+"/tex-mml-chtml.min.js",
 		"11d5c53772a9eb9b95a26d6df0cd8bc4b93d6e305dea4407e611f7e2a7c68657",
